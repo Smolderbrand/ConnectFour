752071cc84310d757d1475ac89de31d6850cff74
Smolderbrand <59597833+Smolderbrand@users.noreply.github.com>
2024-06-08 11:20:00 +0300

    Small changes

--- script.js
+++ script.js
@@ -2,71 +2,76 @@ var currentPlayer;
 var gameState;
 var counts = [];
 var state = [[]];
+var moves;
 
 function start() {
-	window.currentPlayer = 0;
-	window.gameState = 0;
-	window.counts = [0, 0, 0, 0, 0, 0, 0];
-	window.state = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]];
+	currentPlayer = 0;
+	gameState = 0;
+	counts = [0, 0, 0, 0, 0, 0, 0];
+	state = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]];
+	moves = 0;
 }
 
 function clickNew() {
-	window.currentPlayer = 0;
-	window.gameState = 0;
-	window.counts = [0, 0, 0, 0, 0, 0, 0];
+	currentPlayer = 0;
+	gameState = 0;
+	counts = [0, 0, 0, 0, 0, 0, 0];
 	for (var i = 0; i < 6; i++)
 		for (var j = 0; j < 7; j++) {
-			window.state[i][j] = 0;
+			state[i][j] = 0;
 			document.getElementById("button" + String.fromCharCode(65 + j) + String.fromCharCode(65 + i)).textContent = " ";
 		}
+	moves = 0;
 	checkState();
 }
 
 function checkState() {
-	if (window.currentPlayer == 0)
+	if (currentPlayer === 0)
 		document.getElementById("GameStats").textContent = "X to click";
 	else
 		document.getElementById("GameStats").textContent = "O to click";
+	for (var i = 0; i < 3; ++i)
+		for (var j = 0; j < 7; ++j)
+			if (state[i][j] === state[i + 1][j] && state[i][j] === state[i + 2][j] && state[i][j] === state[i + 3][j]) {
+				if (state[i][j] == 1)
+					document.getElementById("GameStats").textContent = "X has won!";
+				else if (state[i][j] == 2)
+					document.getElementById("GameStats").textContent = "O has won!";
+				if (state[i][j] > 0)
+					gameState = 1;
+			}
+	for (var i = 0; i < 6; ++i)
+		for (var j = 0; j < 4; ++j)
+			if (state[i][j] === state[i][j + 1] && state[i][j] === state[i][j + 2] && state[i][j] === state[i][j + 3]) {
+				if (state[i][j] == 1)
+					document.getElementById("GameStats").textContent = "X has won!";
+				else if (state[i][j] == 2)
+					document.getElementById("GameStats").textContent = "O has won!";
+				if (state[i][j] > 0)
+					gameState = 1;
+			}
+	for (var i = 0; i < 3; ++i)
+		for (var j = 0; j < 4; ++j)
+			if (state[i][j] === state[i + 1][j + 1] && state[i][j] === state[i + 2][j + 2] && state[i][j] === state[i + 3][j + 3]) {
+				if (state[i][j] == 1)
+					document.getElementById("GameStats").textContent = "X has won!";
+				else if (state[i][j] == 2)
+					document.getElementById("GameStats").textContent = "O has won!";
+				if (state[i][j] > 0)
+					gameState = 1;
+			}
 	for (var i = 0; i < 3; i++)
-		for (var j = 0; j < 7; j++)
-			if ((window.state[i][j] == window.state[i + 1][j]) && (window.state[i][j] == window.state[i + 2][j]) && (window.state[i][j] == window.state[i + 3][j])) {
-				if (window.state[i][j] == 1)
-					document.getElementById("GameStats").textContent = "X has won!";
-				else if (window.state[i][j] == 2)
-					document.getElementById("GameStats").textContent = "O has won!";
-				if (window.state[i][j] > 0)
-					window.gameState = 1;
-			}
-	for (var i = 0; i < 6; i++)
 		for (var j = 0; j < 4; j++)
-			if ((window.state[i][j] == window.state[i][j + 1]) && (window.state[i][j] == window.state[i][j + 2]) && (window.state[i][j] == window.state[i][j + 3])) {
-				if (window.state[i][j] == 1)
+			if (state[i][j + 3] === state[i + 1][j + 2] && state[i][j + 3] === state[i + 2][j + 1] && state[i][j + 3] === state[i + 3][j]) {
+				if (state[i][j + 3] == 1)
 					document.getElementById("GameStats").textContent = "X has won!";
-				else if (window.state[i][j] == 2)
+				else if (state[i][j + 3] == 2)
 					document.getElementById("GameStats").textContent = "O has won!";
-				if (window.state[i][j] > 0)
-					window.gameState = 1;
+				if (state[i][j + 3] > 0)
+					gameState = 1;
 			}
-	for (var i = 0; i < 3; i++)
-		for (var j = 0; j < 4; j++)
-			if ((window.state[i][j] == window.state[i + 1][j + 1]) && (window.state[i][j] == window.state[i + 2][j + 2]) && (window.state[i][j] == window.state[i + 3][j + 3])) {
-				if (window.state[i][j] == 1)
-					document.getElementById("GameStats").textContent = "X has won!";
-				else if (window.state[i][j] == 2)
-					document.getElementById("GameStats").textContent = "O has won!";
-				if (window.state[i][j] > 0)
-					window.gameState = 1;
-			}
-	for (var i = 0; i < 3; i++)
-		for (var j = 0; j < 4; j++)
-			if ((window.state[i][j + 3] == window.state[i + 1][j + 2]) && (window.state[i][j + 3] == window.state[i + 2][j + 1]) && (window.state[i][j + 3] == window.state[i + 3][j])) {
-				if (window.state[i][j + 3] == 1)
-					document.getElementById("GameStats").textContent = "X has won!";
-				else if (window.state[i][j + 3] == 2)
-					document.getElementById("GameStats").textContent = "O has won!";
-				if (window.state[i][j + 3] > 0)
-					window.gameState = 1;
-			}
+	if ((gameState === 0) && (moves == 42))
+		document.getElementById("GameStats").textContent = "Draw!";
 }
 
 function clickA() {
@@ -98,18 +103,19 @@ function clickG() {
 }
 
 function wrapperButton(number) {
-	if ((window.counts[number] < 6) && (window.gameState == 0)) {
-		if (window.currentPlayer == 0) {
-			window.currentPlayer = 1;
-			window.state[window.counts[number]][number] = 1;
-			document.getElementById("button" + String.fromCharCode(65 + number) + String.fromCharCode(65 + window.counts[number])).textContent = "X";
-			window.counts[number] += 1;
+	if ((counts[number] < 6) && (gameState === 0)) {
+		if (currentPlayer == 0) {
+			currentPlayer = 1;
+			state[counts[number]][number] = 1;
+			document.getElementById("button" + String.fromCharCode(65 + number) + String.fromCharCode(65 + counts[number])).textContent = "X";
+			counts[number] += 1;
 		} else {
-			window.currentPlayer = 0;
-			window.state[window.counts[number]][number] = 2;
-			document.getElementById("button" + String.fromCharCode(65 + number) + String.fromCharCode(65 + window.counts[number])).textContent = "O";
-			window.counts[number] += 1;
+			currentPlayer = 0;
+			state[counts[number]][number] = 2;
+			document.getElementById("button" + String.fromCharCode(65 + number) + String.fromCharCode(65 + counts[number])).textContent = "O";
+			counts[number] += 1;
 		}
+		++moves;
 		checkState();
 	}
 }
